b913b2293608de076388b7e914c1a2684e9bcf13
paulbg <paulbg@hey.com>
2024-05-15 14:36:34 +0300

    feat: implement Hero section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,16 @@
+export const Hero = () => {
+  return (
+    <div className="hero min-h-[75vh]">
+      <div className="hero-content text-center flex-col lg:flex-row-reverse">
+        <div>
+          <h1 className="text-8xl font-black">Welcome to Blooming Luxury!</h1>
+          <p className="text-md py-6">
+            Transform your space with elegance. Discover Bloomie&apos;s
+            exclusive bouquet subscriptions—where every stem tells a story.
+          </p>
+          <button className="btn btn-primary">See Our Plans</button>
+        </div>
+      </div>
+    </div>
+  );
+};
